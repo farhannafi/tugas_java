e5235ae1c9e649fbb6e06623b0763c925235969a
farhannafi <129130814+farhannafi@users.noreply.github.com>
2023-04-11 09:28:55 +0700

    quis PBO JAVA

---
+++ Quis1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author C-13
+ */
+/*package quis1;*/
+
+import java.util.Scanner;
+public class Quis1 {
+
+    public static void main(String[] args) {
+        Scanner penjumlah= new Scanner(System.in);
+        int satu,dua,hasil;
+        System.out.print("Masukan angka ke satu: " );
+        satu = penjumlah.nextInt();
+        System.out.print("Masukan angka ke dua: " );
+        dua = penjumlah.nextInt();
+        hasil=(satu+dua);
+        System.out.println("penjumlahan dari "+ satu +" dan "+ dua+" adalah "+ hasil );
+        System.out.println("Dikerjakan oleh farhan nafi_22201097");
+    }
+    
+}
+
+
+             
+
+
+    
+
+
+
+
+        
+
